c6e12fa72aad354957de55dfabe349fb1ba3cca0
Lybypasta <salaruiuli06@gmail.com>
2023-05-08 14:44:04 +0200

    Update parte 2
    ...

--- lezione00/esercizio02/gruppo dig/main.py
+++ lezione00/esercizio02/gruppo dig/main.py
@@ -6,16 +6,19 @@
 
 
 
-def parte_2 :
+def parte_2():
     risposta_3 = input("E DI BERE QUALCOSA? (sì o no)")
     if risposta_3 == "sì" :
         risposta_3a = int(input("SCEGLI: \n1_ TE' \n2_CAFFE' \n3_CIOCCOLATA?"))
-            if risposta_3a == 1:
-                return "FATEVI 'STO TE'"
-            elif risposta_3a == 2:
-                return "FATEVI 'STO CAFFE'"
-            elif risposta_3a == 3:
-                return "FATEVI 'STA CIOCCOLATA"
+        if risposta_3a == 1:
+            return "FATEVI 'STO TE'"
+        elif risposta_3a == 2:
+            return "FATEVI 'STO CAFFE'"
+        elif risposta_3a == 3:
+            return "FATEVI 'STA CIOCCOLATA"
     else:
-        return no
+        return "no"
+        
+x = parte_2()
+print (x)
     
