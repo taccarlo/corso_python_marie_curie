b0031d2c4792c4031d9a21e6362990950f3f88be
martuz9 <132899293+martuz9@users.noreply.github.com>
2023-05-08 15:24:24 +0200

    Update algoritmo_amicizia.py

--- lezione00/esercizio02/Gruppo caini/algoritmo_amicizia.py
+++ lezione00/esercizio02/Gruppo caini/algoritmo_amicizia.py
@@ -1,1 +1,15 @@
 #Algoritmo dell'amicizia
+#Parte verde
+CONT = 0
+print("Allora svaghiamoci un pò...")
+while CONT<6:
+    print("...cos'altro ti va di fare?")
+    risposta = int(input("E' una cosa che va di fare anche a me? 1)sì 2)no))
+    if risposta=="sì":
+        break
+        print("Facciamolo insieme dai...svaghiamoci un pò insieme")
+    else:
+        CONT = CONT+1
+        print("Fra tutte le opzioni scegli quella meno disumana...fattela piacere, svagatevi un pò insieme.")
+        
+        
