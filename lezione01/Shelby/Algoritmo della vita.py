32da6e61ef020fb23da546795ed14b73d7cf1d57
christiansartori <sartorichristian50@gmail.com>
2023-05-15 13:35:49 +0200

    Update Algoritmo della vita.py

--- lezione01/Shelby/Algoritmo della vita.py
+++ lezione01/Shelby/Algoritmo della vita.py
@@ -44,13 +44,9 @@ def funzione2:
 
 #funzione 3, christian
 def funzione3():
-   if infortunio==0:
-        print("ultimi anni di lavoro")
-   promozioni = int(input("Quante promozioni sono state ottenute nel corso della carriera?")
-   while promozioni >3:
-    elif print("ultimi anni di lavoro")
+    print("ultimi anni di lavoro")
     promozioni = int(input("Quante promozioni sono state ottenute nel corso della carriera?")
     if promozioni >3:
-        print("capo di azienda \n pensione")
-   elif print("pensione")
-return
+        return("capo di azienda")
+    return("pensione")
+
