996bfd69de46d8237dfd8cedde02b40d3c6a754e
christiansartori <sartorichristian50@gmail.com>
2023-05-15 13:15:20 +0200

    Update Algoritmo della vita.py

--- lezione01/Shelby/Algoritmo della vita.py
+++ lezione01/Shelby/Algoritmo della vita.py
@@ -4,4 +4,4 @@ elif print("ultimi anni di lavoro")
 promozioni = int(input("Quante promozioni sono state ottenute nel corso della carriera?")
 if promozioni >3:
 print("capo di azienda \n pensione")
-elif print("pensione"
+elif print("pensione")
