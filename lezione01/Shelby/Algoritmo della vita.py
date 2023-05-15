336edb1e8ff1b84d6c0ba645ad53d8c56314fd31
hu <hugaoyanghg@gmail.com>
2023-05-15 13:48:34 +0200

    Update Algoritmo della vita.py

--- lezione01/Shelby/Algoritmo della vita.py
+++ lezione01/Shelby/Algoritmo della vita.py
@@ -38,12 +38,4 @@ def funzione3():
         return("capo di azienda")
     return("pensione")
 
-
-
-    print("ultimi anni di lavoro")
-    promozioni = int(input("Quante promozioni sono state ottenute nel corso della carriera?"))
-    if promozioni >3:
-        print("capo di azienda")
-    return "pensione"
-
 funzione1()
