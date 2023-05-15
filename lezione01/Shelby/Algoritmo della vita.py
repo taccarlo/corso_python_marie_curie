7e762c78ed585bc6a83a49e13430f1f157fa1464
hu <hugaoyanghg@gmail.com>
2023-05-15 13:14:00 +0200

    Update Algoritmo della vita.py

--- lezione01/Shelby/Algoritmo della vita.py
+++ lezione01/Shelby/Algoritmo della vita.py
@@ -3,8 +3,15 @@ def funzione1():
     print("La persona segtue le elementari e le medie")
     n_bocciature=0
     anno_superiori=0
+    n_bocciature=0
     while True:
         print(f'segue il {anno_superiori} anno delle superiori')
         anno_superiori+=1
         bocciature=int(input("Sei stato bocciato, se sì digita 1, altrimenti 0?")
-        if bocciature 
+        if bocciature == 1:
+            n_bocciature+=1
+            if n_bocciature > 3:
+                return funzione2
+        else:
+            if anno_superiori == 5:
+                return funzione2
