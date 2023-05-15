dcf868facb3603a9da519a68f1408ad22d7d493f
dennis607 <132899198+dennis607@users.noreply.github.com>
2023-05-15 13:28:06 +0200

    Update algoritmo_della_vita.py

--- lezione01/Momoz_Dennis_e_Mary/algoritmo_della_vita.py
+++ lezione01/Momoz_Dennis_e_Mary/algoritmo_della_vita.py
@@ -1,26 +1,10 @@
-#algoritmo della vita
-#riquadro giallo
-
-def Funzione_1:
-    print("La persona è nata!")
-    print("La persona segue le elementari e le medie.")
-    n_bocciature = 0
-    anno_superiori = 0
-    N = 0
-    print(f"Seguo il {N} anno delle superiori.")
-    anno_superiori = anno_superiori+1
-    while True:
-        bocciatura = int(input("Sarò bocciato? 0=sì 1=no"))
-        if bocciatura == 0:
-            n_bocciature = n_bocciature+1
-            if n_bocciature>3:
-                print("Meglio se vado a lavorare.")
-        else:
-            anno_superiori = anno_superiori+1
-            if anno_superiori ==5:
-                print("Finalmente vado a lavorare")
-        
-        
-        
-        
+def Funzione_3:
+    elif print("ultimi anni di lavoro")
+    promozioni = int(input("Quante promozioni sono state ottenute nel corso della carriera?")
+    if promozioni >3:
+        print("capo di azienda")
+                     print("pensione")
+        elif print("pensione")
+return
+            
     
