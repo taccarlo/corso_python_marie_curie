d71fc3b78324a4e7af29961a0d7c31124d93d236
Carlo Tacchella <tacchella.carlo@gmail.com>
2023-05-15 14:00:51 +0200

    Update algoritmo_della_vita.py

--- lezione01/Momoz_Dennis_e_Mary/algoritmo_della_vita.py
+++ lezione01/Momoz_Dennis_e_Mary/algoritmo_della_vita.py
@@ -1,20 +1,17 @@
-<<<<<<< HEAD
 
-            
-=======
 #algoritmo della vita
 #riquadro giallo
 
-def Funzione_1:
+def Funzione_1():
     print("La persona è nata!")
     print("La persona segue le elementari e le medie.")
     n_bocciature = 0
     anno_superiori = 0
     while True:
-    N = 1
-    print(f"Seguo il {N} anno delle superiori.")
-    anno_superiori = anno_superiori+1
-    bocciatura = int(input("Sarò bocciato? 0=sì 1=no"))
+        N = 1
+        print(f"Seguo il {N} anno delle superiori.")
+        anno_superiori = anno_superiori+1
+        bocciatura = int(input("Sarò bocciato? 0=sì 1=no"))
         if bocciatura==0:
             n_bocciature = n_bocciature+1
             if n_bocciature>3:
@@ -24,10 +21,10 @@ def Funzione_1:
             if anno_superiori==5:
                 print("Finalmente vado a lavorare")
         N = N+1
-        return Funzione_2
+    return Funzione_2()
 
 def Funzione_2:
-Anni di lavoro=0
+print(Anni di lavoro=0)
 while Anni di lavoro < 20:
     Anni di lavoro = Anni di lavoro + 1
     print("L'anno lavorativo si conclude")
@@ -47,5 +44,3 @@ print("pensione")
 return       
         
         
->>>>>>> 
-    
