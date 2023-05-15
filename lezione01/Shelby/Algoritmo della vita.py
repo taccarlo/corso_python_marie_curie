b70764f01c1def16495e4dbc8587c9b37c9436e5
hu <hugaoyanghg@gmail.com>
2023-05-15 13:47:46 +0200

    Update Algoritmo della vita.py

--- lezione01/Shelby/Algoritmo della vita.py
+++ lezione01/Shelby/Algoritmo della vita.py
@@ -1,4 +1,3 @@
-
 def funzione1():
     print("La persona è nata!")
     print("La persona segue le elementari e le medie")
@@ -7,38 +6,27 @@ def funzione1():
     while True:
         print(f'segue il {anno_superiori} anno delle superiori')
         anno_superiori+=1
-        bocciature=int(input("Sei stato bocciato, se sì digita 1, altrimenti 0?")
-        if bocciature == 1:
+        bocciature=int(input("Sei stato bocciato, se sì digita 1, altrimenti 0?"))
+        if bocciature ==1:
             n_bocciature+=1
-            if n_bocciature > 3:
-                return funzione2
+            if n_bocciature >= 3:
+                return funzione2()
         else:
-            if anno_superiori == 5:
-                return funzione2
-                
-
-               
-               
+            if anno_superiori >= 5:
+                return funzione2()
+                         
                
 def funzione2():
     lavoro=0
     
-    while lavoro<=20:
+    while lavoro<20:
         lavoro=lavoro+1
         print(lavoro, "anni di lavoro")
     infortunio=int(input("presenza di infortunio delibitante? se si digita 1 altrimenti digita 0"))
     if infortunio==1:
-        return "pensione" 
+        print ("pensione")
     else:
-        return funzione3
-
-
-
-
-
-
-
-
+        return funzione3()
 
 
 
@@ -46,7 +34,9 @@ def funzione2():
 def funzione3():
 
     print("ultimi anni di lavoro")
-    promozioni = int(input("Quante promozioni sono state ottenute nel corso della carriera?")
+    promozioni = int(input("Quante promozioni sono state ottenute nel corso della carriera?"))
     if promozioni >3:
-        return "capo di azienda"
+        print("capo di azienda")
     return "pensione"
+
+funzione1()
